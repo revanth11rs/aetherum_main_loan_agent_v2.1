9ec180cd452292a96f246f20fa70105f8d30f82e
revanth11rs <revanth@aetherum.ai>
2025-08-31 14:22:36 -0700

    Initial commit

--- src/utils/config.py
+++ src/utils/config.py
@@ -7,7 +7,7 @@ class Settings:
     METRICS_API_BASE: str = os.getenv("METRICS_API_BASE", "http://localhost:5002")
     AI_PROVIDER: str = os.getenv("AI_PROVIDER", "groq")
     AI_MODEL_NAME: str = os.getenv("AI_MODEL_NAME")
-    # AI_MODEL_NAME: str = os.getenv("AI_MODEL_NAME", "llama3-70b-8192")
+    AI_MODEL_NAME: str = os.getenv("AI_MODEL_NAME", "llama3-70b-8192")
     AI_MODEL_TEMPERATURE: float = float(os.getenv("AI_MODEL_TEMPERATURE", "0.2"))
     AI_MODEL_MAX_TOKENS: int = int(os.getenv("AI_MODEL_MAX_TOKENS", "2048"))
     AI_MODEL_TOP_P: float = float(os.getenv("AI_MODEL_TOP_P", "0.95"))
